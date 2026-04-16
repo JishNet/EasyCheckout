307c71c1f16cbbb41b235340c08650fb9c1af0ac
JishNet <jishan6263487341@gmail.com>
2026-04-16 17:42:07 +0530

    add cors configuration

---
+++ src/main/java/com/example/EasyCheckout/Config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.example.EasyCheckout.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://10.67.86.134:5500",
+                                "http://localhost:5500"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
--- src/main/java/com/example/EasyCheckout/Controller/PaymentController.java
+++ src/main/java/com/example/EasyCheckout/Controller/PaymentController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
 
 @RestController
 @RequestMapping("/api/payment")
